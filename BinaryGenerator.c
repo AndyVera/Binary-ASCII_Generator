ea219bb2a895edf6a74abf7dbff3ce13b9fc02d5
AndyVera <42859114+AndyVera@users.noreply.github.com>
2020-03-10 20:47:55 -0700

    Update BinaryGenerator.c

--- BinaryGenerator.c
+++ BinaryGenerator.c
@@ -37,6 +37,29 @@ char* bin2a(size_t n){
     return buff;
 }
 
+//This function is commented out but
+//can be used instead of the above one
+//to generate a number at different bases.
+//COMMENT THE OTHER ONE ABOVE OUT IF GENERATING NUMBERS USING DIFFERENT BASES
+
+// char* bin2a(size_t n, int base){
+//     static char buff[MAXLEN];
+//     size_t idx = 0;
+//     memset(buff, 0, sizeof(buff));
+    
+//     while(n > 0){
+//         size_t rem = n % base;
+//         n /= base;
+//         buff[idx++] = rem + '0';
+//     }
+//     size_t len = strlen(buff);
+//     for(size_t i = 0; i < 8 - len; ++i){
+//         buff[len + i] = '0';
+//     }
+//     str_reverse(buff);
+//     return buff;
+// }
+
 
 int main(int argc, const char* argv[]){
     if(argc != 2){
@@ -45,8 +68,20 @@ int main(int argc, const char* argv[]){
     }
     FILE* fout = fopen(argv[1], "w");
     
+// These nested loops can only be used when wantinf to 
+// generate numbers based of different bases
+//IF WANT TO GENERATE USING DIFFERNT BASES USE THIS ONE
+//COMMENT OUT THE ONE BELOW
 //    for(size_t base  = 2; base <= 10; ++base){
 //        fprintf(fout, "Base %zu ---------------------------------------------------------------\n", base);
+//          for(size_t number = 0; number <= 127; ++number){
+//              fprintf(fout,"%s ", bin2a(number, base));
+//                   if( number % 8 == 7){
+//                   fprintf(fout, "\n");
+//               }
+//           }
+//      }
+   
         
     for(size_t number = 0; number <= 127; ++number){
         fprintf(fout,"%s ", bin2a(number));
